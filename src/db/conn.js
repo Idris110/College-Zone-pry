cf19fc16139d19fb2302937d1a51e713ff34c6f5
Idris110 <67634769+Idris110@users.noreply.github.com>
2022-04-28 01:30:42 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/ClubZone", {
+  .connect("mongodb+srv://user1:1234@clubzone.12dvv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
